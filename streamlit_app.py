ca4b317c8639076718ca8dfa4cc8048506fe591d
souissialaa <91916670+souissialaa@users.noreply.github.com>
2024-01-10 18:53:25 +0100

    Update streamlit_app.py
    Fixed error on text 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale',Spinach & Rocket Smoothie')
+st.text('Kale,Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
         
